845f0f7e97de439b03e5a883817facb0dd5d1be1
dcash7 <dcash@bryant.edu>
2018-01-14 22:46:05 -0500

    done

---
+++ app/src/main/java/com/londonappbrewery/bitcointicker/ParseJSON.java
@@ -0,0 +1,29 @@
+package com.londonappbrewery.bitcointicker;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by student on 1/14/2018.
+ */
+
+public class ParseJSON {
+
+    private String mPrice;
+
+    public static ParseJSON fromJSON(JSONObject jsonObject){
+        ParseJSON JSON = new ParseJSON();
+        try {
+            JSON.mPrice = jsonObject.getString("ask");
+            return JSON;
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+    public String getPrice(){
+        return mPrice;
+    }
+}
